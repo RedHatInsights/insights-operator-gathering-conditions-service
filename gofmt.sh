cd2c6b0c19549c61961ef19d32101e9c1c1f0b09
Juan Diaz Suarez <jdiazsua@redhat.com>
2022-01-03 16:39:28 +0100

    pass shellcheck

--- gofmt.sh
+++ gofmt.sh
@@ -21,7 +21,7 @@ NC=$(tput sgr0) # No Color
 
 echo -e "${BLUE}Running formatting tool for Go source code${NC}"
 
-if [ -n "$(go fmt $(go list ./... | grep -v /vendor/))" ]
+if [ -n "$(go fmt "$(go list ./... | grep -v /vendor/)")" ]
 then
     echo -e "${RED_BG}[FAIL]${NC} Go code is not formatted:"
     gofmt -d .
--- gosec.sh
+++ gosec.sh
@@ -48,7 +48,7 @@ then
     fi
 fi
 
-if ! gosec $GO_SEC_ARGS $(go list ./... | grep -v /vendor/)
+if ! gosec $GO_SEC_ARGS "$(go list ./... | grep -v /vendor/)"
 then
     echo -e "${RED_BG}[FAIL]${NC} Potential security issues detected!"
     exit 1
