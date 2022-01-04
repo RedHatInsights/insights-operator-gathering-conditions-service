df549fd14ce5b8be2fb13c99725bcb3cdcab2a7e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-04 12:11:02 +0100

    Fixed gocyclo

--- gocyclo.sh
+++ gocyclo.sh
@@ -28,7 +28,7 @@ then
     GO111MODULE=off go get github.com/fzipp/gocyclo/cmd/gocyclo
 fi
 
-if ! gocyclo -over 10 -avg cmd internal
+if ! gocyclo -over 10 -avg main.go internal
 then
     echo -e "${RED_BG}[FAIL]${NC} Functions/methods with high cyclomatic complexity detected"
     exit 1
