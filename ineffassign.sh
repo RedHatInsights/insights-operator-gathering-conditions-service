9bad9c1c753a04ab90805cb5d4aff10e561c303e
Pavel Tišnovský <ptisnovs@redhat.com>
2022-01-05 11:09:31 +0100

    Corrected path

--- ineffassign.sh
+++ ineffassign.sh
@@ -27,7 +27,7 @@ then
     GO111MODULE=off go get github.com/gordonklaus/ineffassign
 fi
 
-if ! ineffassign main.go internal/
+if ! ineffassign github.com/redhatinsights/insights-operator-conditional-gathering
 then
     echo -e "${RED_BG}[FAIL]${NC} Code with ineffectual assignments detected"
     exit 1
