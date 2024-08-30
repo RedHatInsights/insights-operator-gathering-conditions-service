2c2284a88ae6c5df67f75642069bd124582ebe99
Juan Díaz <jdiazsua@redhat.com>
2024-08-30 07:50:17 +0200

    [CCXDEV-13089] Add a prometheus metric with the remote configurations path that is returned to the client (#269)
    * add a prometheus metric with the remote configurations path that is returned
    
    * use different registries each time in order to fix UTs
    
    * add prometheus exporter

---
+++ internal/service/metrics.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	remoteConfigurationsMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "io_gathering_remote_configuration",
+			Help: "The number of times a remote configuration was returned",
+		},
+		[]string{"file"})
+)
