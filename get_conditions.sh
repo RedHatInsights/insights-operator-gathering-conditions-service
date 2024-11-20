d24d74983b834a6f57a3c1a743e2997edec7d18b
Jan Holeček <71874510+jholecek-rh@users.noreply.github.com>
2024-11-20 08:57:43 +0100

    Set config canary version to 1.1.1
    Config version 1.1.1 is identical to 1.1.0. The purpose of this change is
    to validate the release process documentation without actually changing
    anything.

--- get_conditions.sh
+++ get_conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STABLE_VERSION=1.1.0
-CANARY_VERSION=1.1.0
+CANARY_VERSION=1.1.1
 
 # Clone the conditions repo and build it to gather the conditions
 if [ ! -d 'insights-operator-gathering-conditions' ]; then git clone https://github.com/RedHatInsights/insights-operator-gathering-conditions; fi
