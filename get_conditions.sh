9765dc27ffa4e75997c8a7adf9726a50bc054b72
Iker Reyes <ireyes@redhat.com>
2025-03-19 11:57:43 +0100

    Stop get_conditions script if failure
    Replace existing commands with a version that fails if the script fails at any point.

--- get_conditions.sh
+++ get_conditions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 STABLE_VERSION=1.1.3
 CANARY_VERSION=1.1.3
@@ -9,18 +10,18 @@ mkdir -p conditions
 mkdir -p remote-configurations
 mkdir -p mapping/stable
 mkdir -p mapping/canary
-cd insights-operator-gathering-conditions || exit 1
+cd insights-operator-gathering-conditions
 
-git checkout ${STABLE_VERSION} && \
-./build.sh && \
-cp -r build/v1 ../conditions/stable && \
-cp -r build/v2 ../remote-configurations/stable && \
-cp build/cluster-mapping.json ../mapping/stable/ && \
-rm -r build ; \
+git checkout ${STABLE_VERSION}
+./build.sh
+cp -r build/v1 ../conditions/stable
+cp -r build/v2 ../remote-configurations/stable
+cp build/cluster-mapping.json ../mapping/stable/
+rm -r build
 
-git checkout ${CANARY_VERSION} && \
-./build.sh && \
-cp -r build/v1 ../conditions/canary && \
-cp -r build/v2 ../remote-configurations/canary && \
-cp build/cluster-mapping.json ../mapping/canary/ && \
-rm -r build ; \
+git checkout ${CANARY_VERSION}
+./build.sh
+cp -r build/v1 ../conditions/canary
+cp -r build/v2 ../remote-configurations/canary
+cp build/cluster-mapping.json ../mapping/canary/
+rm -r build
