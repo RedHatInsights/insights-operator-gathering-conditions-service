19d350beafafa8a54bf4c9569ce6c536c7f6c69e
Iker Reyes <ireyes@redhat.com>
2025-03-28 13:33:58 +0100

    Drop legacy building
    Now that a new build script has been implemented in the gathering conditions repo
    and it is being used for all versions, we can remove the old code related to the legacy building system.

--- get_conditions.sh
+++ get_conditions.sh
@@ -16,14 +16,6 @@ build() {
   rm -r build
 }
 
-build_legacy() {
-  ./build.sh
-  cp -r build/v1 "../conditions/$1"
-  cp -r build/v2 "../remote-configurations/$1"
-  cp build/cluster-mapping.json "../remote-configurations/$1/cluster_version_mapping.json"
-  rm -r build
-}
-
 # Clone the conditions repo and build it to gather the conditions
 if [ ! -d 'insights-operator-gathering-conditions' ]; then git clone https://github.com/RedHatInsights/insights-operator-gathering-conditions; fi
 mkdir -p conditions
@@ -35,17 +27,9 @@ python3.11 -m venv .env
 source .env/bin/activate
 
 git checkout ${STABLE_VERSION}
-if [ -f build.py ]; then
-  build "stable"
-else
-  build_legacy "stable"
-fi
+build "stable"
 
 git checkout ${CANARY_VERSION}
-if [ -f build.py ]; then
-  build "canary"
-else
-  build_legacy "canary"
-fi
+build "canary"
 
 rm -r .env
